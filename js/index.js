62322f53e8df47ac0ce5ef01261269d1b19e880b
Ammar Bagasrawala <ammarb27@gmail.com>
2016-09-02 12:34:09 +1200

    Uploaded image now shows up

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 var person_name = $("#person-name")[0];
 var file_list = $("#file-list")[0];
-var selected_image = $("#selected_image")[0];
-var status_message = $("#status_message")[0];
+var status_message = $("#status-message")[0];
+var selected_image = $("#selected-image")[0];
+var features = "?visualFeatures=Description&details=Celebrities";
 file_list.addEventListener("change", function () {
+    status_message.innerHTML = "Please wait while we retreive the information";
     processImage(function (file) {
         sendRequest(file, function () {
             console.log("Info retrieved");
@@ -11,7 +13,7 @@ file_list.addEventListener("change", function () {
 });
 function sendRequest(file, callback) {
     $.ajax({
-        url: "https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Description&details=Celebrities",
+        url: "https://api.projectoxford.ai/vision/v1.0/analyze" + features,
         beforeSend: function (xhrObj) {
             xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "af21cfa294ff43529375c4ef3c6debb6");
@@ -22,7 +24,6 @@ function sendRequest(file, callback) {
     })
         .done(function (data) {
         if (data) {
-            console.log(data.categories[0].detail.celebrities[0].name);
             console.log(data);
         }
         else {
@@ -42,7 +43,7 @@ function processImage(callback) {
     else {
         console.log("Invalid file");
     }
-    reader.onloadend = function () {
+    reader.onloadend = function (e) {
         if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
             status_message.innerHTML = "Please upload an image file (jpg or png).";
         }
@@ -50,4 +51,13 @@ function processImage(callback) {
             callback(file);
         }
     };
+}
+function readUrl(input) {
+    if (input.files) {
+        var image_reader = new FileReader();
+        image_reader.onload = function (e) {
+            $("#selected-image").attr("src", e.target.result);
+        };
+        image_reader.readAsDataURL(input.files[0]);
+    }
 }
--- js/index.ts
+++ js/index.ts
@@ -1,10 +1,11 @@
 var person_name = $("#person-name")[0];
 var file_list : HTMLInputElement = <HTMLInputElement> $("#file-list")[0];
-var selected_image = $("#selected_image")[0];
-var status_message = $("#status_message")[0];
+var status_message = $("#status-message")[0];
+var selected_image = $("#selected-image")[0];
+var features : String = "?visualFeatures=Description&details=Celebrities";
 
 file_list.addEventListener("change", function () {
-  // status_message.innerHTML = "Please wait while we retreive the information";
+  status_message.innerHTML = "Please wait while we retreive the information";
   processImage(function (file) {
     sendRequest(file, function() {
       console.log("Info retrieved");
@@ -14,7 +15,7 @@ file_list.addEventListener("change", function () {
 
 function sendRequest(file, callback) : void{
   $.ajax({
-    url: "https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Description&details=Celebrities",
+    url: "https://api.projectoxford.ai/vision/v1.0/analyze" + features,
     beforeSend: function(xhrObj) {
       // Headers
       xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
@@ -26,7 +27,9 @@ function sendRequest(file, callback) : void{
   })
     .done(function (data) {
       if (data) {
-        console.log(data.categories[0].detail.celebrities[0].name);
+        //var name : String = data.categories[0].detail.celebrities[0].name;
+        //var description : String = data.description.captions[0].text;
+        //console.log(name + ", " + description);
         console.log(data);
       } else {
         console.log("something went wrong");
@@ -45,11 +48,21 @@ function processImage(callback) : void {
   } else {
     console.log("Invalid file");
   }
-  reader.onloadend = function () {
+  reader.onloadend = function (e) {
     if (!file.name.match(/\.(jpg|jpeg|png)$/)) {
       status_message.innerHTML = "Please upload an image file (jpg or png).";
     } else {
       callback(file);
     }
+  }
+}
+
+function readUrl (input): void {
+  if (input.files) {
+    var image_reader = new FileReader();
+    image_reader.onload = function (e) {
+      $("#selected-image").attr("src", e.target.result);
+    };
+    image_reader.readAsDataURL(input.files[0]);
   }
 }
